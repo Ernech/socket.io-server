a670cc19d3984d3aced27796d94b931cb664844d
unknown <ernesto.vilela.montero@gmail.com>
2021-07-03 17:48:03 -0400

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,4 +1,17 @@
-const {io} = require('../index');
+const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+
+const bands = new Bands();
+console.log('Init server');
+
+bands.addBand(new Band('Opeth'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Within Temptation'));
+bands.addBand(new Band('Arch Enemy'));
+
+console.log(bands);
 
 //Mensajes de sockets
 
@@ -6,11 +19,29 @@ const {io} = require('../index');
 
 io.on('connection', client => {
     console.log('Cliente conectado');
-    client.on('disconnect', () => {console.log('Cliente desconectado'); });
 
-    client.on('mensaje',(payload)=>{
+    client.emit('active-bands', bands.getBands());
+    client.on('disconnect', () => { console.log('Cliente desconectado'); });
 
-        console.log('Mensaje!!!',payload);
-        io.emit('mensaje',{admin:'Nuevo mensaje'});
+    client.on('mensaje', (payload) => {
+
+        console.log('Mensaje!!!', payload);
+        io.emit('mensaje', { admin: 'Nuevo mensaje' });
     });
-  });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+            const newBand = new Band(payload.name);
+            bands.addBand(newBand);
+            io.emit('active-bands', bands.getBands());            
+    });
+    client.on('delete-band', (payload) => {
+       bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());            
+});
+
+});
